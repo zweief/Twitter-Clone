242526efdb53f3e7d222a8fd223a6fba6eaddc38
Florian Frenzel <zweief@users.noreply.github.com>
2017-08-21 22:46:28 +0200

    add singeup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
   get '/about',   to: 'static_pages#about'
   get '/help',    to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
+  get '/signup',  to: 'users#new'
 end
